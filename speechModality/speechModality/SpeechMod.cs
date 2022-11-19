4fdcb32ee132c3db147f85647a854e7363662d8e
Simão <sdgbentes@gmail.com>
2022-11-19 14:40:43 +0000

    grammar fix and receive message tts

--- speechModality/speechModality/SpeechMod.cs
+++ speechModality/speechModality/SpeechMod.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using mmisharp;
 using Microsoft.Speech.Recognition;
 using System.Xml.Linq;
-//using Newtonsoft.Json;
+using Newtonsoft.Json;
 
 namespace speechModality
 {
@@ -55,7 +55,7 @@ namespace speechModality
             sre.SpeechHypothesized += Sre_SpeechHypothesized;
 
             // NEW - TTS support 16 April
-            tts.Speak("Olá. Estou pronto para receber ordens.");
+            tts.Speak("Olá. Pronto para ver Netflix?");
 
 
             //  o TTS  no final indica que se recebe mensagens enviadas para TTS
@@ -76,6 +76,9 @@ namespace speechModality
         private void Sre_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
         {
             onRecognized(new SpeechEventArg() { Text = e.Result.Text, Confidence = e.Result.Confidence, Final = true });
+
+
+           
 
             //SEND
             // IMPORTANT TO KEEP THE FORMAT {"recognized":["SHAPE","COLOR"]}
@@ -109,44 +112,50 @@ namespace speechModality
             var doc = XDocument.Parse(e.Message);
             var com = doc.Descendants("command").FirstOrDefault().Value;
 
+            Console.WriteLine("arr");
             Console.WriteLine(com);
 
 
-            tts.Speak(com);
 
-            //dynamic json = JsonConvert.DeserializeObject(com);
+            dynamic json = JsonConvert.DeserializeObject(com);
 
-            /*
-            Shape _s = null;
-            switch ((string)json.recognized[0].ToString())
+       
+            switch ((string)json.synthesize[0].ToString())
             {
-                case "SQUARE":
-                    _s = rectangle;
+                case "OPEN DONE.":
+                    tts.Speak("Bem-vindo. Estes são os perfis disponíveis. Que conta deseja utilizar?");
                     break;
-                case "CIRCLE":
-                    _s = circle;
+                case "FILM ACTION DONE.":
+                    tts.Speak("Estes são os filmes de ação disponíveis.");
                     break;
-                case "TRIANGLE":
-                    _s = triangle;
+                case "FILM COMEDY DONE.":
+                    tts.Speak("Estes são os filmes de comédia disponíveis.");
+                    break;
+                case "FILM DRAMA DONE.":
+                    tts.Speak("Estes são os filmes de drama disponíveis.");
+                    break;
+                case "FILM NOVEL DONE.":
+                    tts.Speak("Estes são os filmes de romance disponíveis.");
+                    break;
+                case "FILM TERROR DONE.":
+                    tts.Speak("Estes são os filmes de terror disponíveis.");
+                    break;
+                case "SERIES ACTION DONE.":
+                    tts.Speak("Estas são as séries de ação disponíveis.");
+                    break;
+                case "SERIES COMEDY DONE.":
+                    tts.Speak("Estas são as séries de comédia disponíveis.");
+                    break;
+                case "SERIES DRAMA DONE.":
+                    tts.Speak("Estas são as séries de drama disponíveis.");
+                    break;
+                case "SERIES NOVEL DONE.":
+                    tts.Speak("Estas são as séries de romance disponíveis.");
+                    break;
+                case "SERIES TERROR DONE.":
+                    tts.Speak("Estas são as séries de terror disponíveis.");
                     break;
             }
-
-            App.Current.Dispatcher.Invoke(() =>
-            {
-                switch ((string)json.recognized[1].ToString())
-                {
-                    case "GREEN":
-                        _s.Fill = Brushes.Green;
-                        break;
-                    case "BLUE":
-                        _s.Fill = Brushes.Blue;
-                        break;
-                    case "RED":
-                        _s.Fill = Brushes.Red;
-                        break;
-                }
-            });
-            */
 
 
         }
